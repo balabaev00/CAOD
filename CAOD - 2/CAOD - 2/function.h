348e2c6e4f56b9073ac699de8bbfa11a4c4e6bfb
balabaev00 <balabaev00@yandex.ru>
2020-02-09 12:46:14 +0900

    create

---
+++ CAOD - 2/CAOD - 2/function.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+/*xBegin - xEnd это интервал
+a, b, c - действительные числа
+dX - шаг
+*/
+void function(double a, double b, double c, double xBegin, double xEnd, double dX); // Функция 
