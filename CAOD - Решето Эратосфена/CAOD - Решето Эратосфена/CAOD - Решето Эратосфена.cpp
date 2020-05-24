f4474e237f4717e5640667a29b79972c674f1884
balabaev00 <balabaev00@yandex.ru>
2020-05-24 16:08:18 +0900

    add new project

---
+++ CAOD - Решето Эратосфена/CAOD - Решето Эратосфена/CAOD - Решето Эратосфена.cpp
@@ -0,0 +1,32 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	ofstream out;
+	out.open("result.txt");
+	cout << "n= ";
+	cin >> n;
+	int *a = new int[n + 1];
+	for (int i = 0; i < n + 1; i++)
+		a[i] = i;
+	out << "n = " << n << endl;
+	for (int p = 2; p < n + 1; p++)
+	{
+		if (a[p] != 0)
+		{
+			cout << a[p] << endl;
+			out << a[p] << endl;
+			for (int j = p * p; j < n + 1; j += p)
+				a[j] = 0;
+		}
+	}
+	delete a;
+	out.close();
+	system("pause");
+}
